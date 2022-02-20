c3c49c90bc63b56975436a2fbc75d5ebe81d5d77
luoxiaojun <luoxiaojun1992@sina.cn>
2022-02-20 23:15:58 +0800

    feat: add linter command

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"github.com/luoxiaojun1992/go-missing-len/pkg"
+	"go/parser"
+	"go/token"
+)
 
+func main() {
+	var filename string
+	flag.StringVar(&filename, "file", "", "")
+	flag.Parse()
+
+	file, err := parser.ParseFile(token.NewFileSet(), filename, nil, 0)
+	if err != nil {
+		panic(err)
+	}
+	linter := pkg.NewMissingLenLinter()
+	linter.Check(file)
+	if len(linter.Hints) > 0 {
+		for _, hint := range linter.Hints {
+			fmt.Printf("Pos: %d, End: %d, Category: %s, Message: %s, Suggestion: %s \n", hint.Pos, hint.End, hint.Category, hint.Message, hint.Suggestion)
+		}
+	}
 }
---
+++ testdata/sample.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	s := make([]int, 0)
+	s1 := []int{1, 2, 3}
+	for _, num := range s1 {
+		s = append(s, num)
+	}
+}
